da956f41f763775a1b8b4b663edcd175fdd28dd6
Origaska <lucio.hernandez.noguez@gmail.com>
2021-12-08 12:22:30 -0600

    Agregando Eventos

--- app/src/main/java/com/team/proyectosmarthf/MainActivity.java
+++ app/src/main/java/com/team/proyectosmarthf/MainActivity.java
@@ -1,14 +1,77 @@
 package com.team.proyectosmarthf;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txtporcentaje;
+    private TextView txtminimos;
+    private TextView txtmaximos;
+
+    private ProgressBar progreso;
+
+    private SeekBar barraMinimos;
+    private SeekBar barraMaximos;
+
+    private FloatingActionButton btnActualizar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inicializarComponentes();
+
+        btnActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actualizar();
+            }
+        });
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menuGuardar){
+            /*Guardar Cambios*/
+
+
+
+            Toast.makeText(this, "Cambios Aplicados correctamente",Toast.LENGTH_SHORT);
+        }
+
+
+        return true;
+    }
+
+    private void actualizar() {
+        /*Actualizar porcentaje de la base de datos*/
+
+    }
+
+    private void inicializarComponentes(){
+        txtminimos = (TextView) findViewById(R.id.txtmin);
+        txtmaximos = (TextView) findViewById(R.id.txtmax);
+        txtporcentaje = (TextView) findViewById(R.id.textView);
+        progreso = (ProgressBar) findViewById(R.id.barraProgreso);
+        barraMinimos = (SeekBar) findViewById(R.id.seekBarMinimo);
+        barraMaximos = (SeekBar) findViewById(R.id.seekBarMax);
+        btnActualizar = (FloatingActionButton) findViewById(R.id.btnRefresh);
+    }
+
+
+
 }
